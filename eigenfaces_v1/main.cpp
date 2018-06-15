ed2a76f9b9f07059ece62604d529893fb78ecf7e
Max Rumpf <max.rumpf@me.com>
2018-06-15 18:18:48 +0200

    Initial Commit

---
+++ eigenfaces_v1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  eigenfaces_v1
+//
+//  Created by Max Rumpf on 15.06.18.
+//  Copyright © 2018 Max Rumpf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
